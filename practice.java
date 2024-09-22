8eb02899ea8264f40bf06e65fd1fa6bec86cabef
dipakp2793 <dipakp2793@gmail.com>
2024-09-22 15:19:15 +0530

    first commit

---
+++ practice.java
@@ -0,0 +1,33 @@
+package dipak_Drogenide;
+
+import java.util.Arrays;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+ 
+         int a=0;
+         int b=1;
+         int c;
+         
+         for(int i=0;i<=15;i++) {
+        	 c=a+b;
+        	 
+        	 System.out.println(c);
+        	 
+        	 a=b;
+        	 b=c;
+        	 
+        	 
+        	 
+         }
+        		 
+         
+
+		
+		}
+	}
+
+
